b0326928768e349711e54fe3894aa35d1812cc0e
Iudà <86917292+IUDA194@users.noreply.github.com>
2024-02-11 14:10:03 +0100

    cors

--- hpcc_marks/settings.py
+++ hpcc_marks/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'marks',
     'users',
+    'corsheaders',
     'logs'
 ]
 
@@ -58,6 +59,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -81,6 +84,39 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hpcc_marks.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    'http://185.107.237.179',
+    'http://185.107.237.179:8000',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    'http://185.107.237.179',
+    'http://185.107.237.179:8000'
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
